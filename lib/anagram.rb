90cb357139ed45215c156ab02de4c733397d00d9
Ethan Isola <isolaethan@gmail.com>
2022-02-23 21:44:37 -0600

    commit tree 06bbbe5dd1effc735b3f5ef8f72baf23bffb30ba

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_reader :sentence, :word
+    @final_words = []
+    def initialize(word)
+        @word = word
+    end
+    def match(sentence)
+        @final_words = sentence.filter do |my_word|
+            my_word.chars.sort === word.chars.sort
+        end
+    end
+end
